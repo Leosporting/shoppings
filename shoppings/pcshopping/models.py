48b9af369ba52a8057a920f9c30ff1180cd39438
PaulWangretair <magicryze@mailc.om>
2017-01-07 01:39:39 +0800

    update

--- shoppings/pcshopping/models.py
+++ shoppings/pcshopping/models.py
@@ -10,17 +10,24 @@ class  Category(models.Model):
 
 
 class item(models.Model):
-    category = models.TextField(max_length=100, default=None)
+    category=Category()
+    #category = models.TextField(max_length=100, default=None)
     item_name = models.CharField(verbose_name='商品名稱', max_length=20)
     item_description = models.TextField(verbose_name='商品描述',max_length=100)
     item_price = models.PositiveIntegerField(verbose_name='商品價格',default=0)
     created_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.item_name
+
 class member(models.Model):
     member_name=models.CharField(max_length=20)
     member_account = models.CharField(max_length=8,blank=True)
     member_phone=models.PositiveIntegerField(blank=True,default=None)
+
+    def __str__(self):
+        return self.member_name
 
 
 
--- shoppings/pcshopping/views.py
+++ shoppings/pcshopping/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from datetime import datetime
 from .models import Category,item
 
+
+
+
+
 def  test_pc_shoppings(request):
     return HttpResponse(datetime.now())
    # return render(request, 'hello_world.html', {
@@ -15,7 +19,7 @@ def pc_shoppings_list_view(request, category=None):
     pc_shoppings_list_queryset =item.objects.all()
     if category:
         pc_shoppings_list_queryset = pc_shoppings_list_queryset.filter(category=category)
-    return render("shoppings/pc_shoppings_list.html",{'pc_shoppings_list': pc_shoppings_list_queryset,'category': Category.objects.all() })
+    return render("shoppings/pc_shoppings_list.html",{'pc_shoppings_list': pc_shoppings_list_queryset,'category': category.objects.all() })
 
 
 
