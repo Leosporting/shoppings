1c166e97418a226bd2f0e910f168f6cac5bd3910
paulWangRetair <magicryze@gmail.com>
2017-01-10 20:38:17 +0800

    add    heroku

--- shoppings/shoppings/settings.py
+++ shoppings/shoppings/settings.py
@@ -14,6 +14,9 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 #os.path.abspath(__file__)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
 print (BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
@@ -102,3 +105,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
